08ee1d5504a72b924f5c992384b63323b364df37
Harsha Vardhan Reddy <harshavennapusa2006@gmail.com>
2025-07-24 22:32:55 +0530

    done with dashboard API

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const connectDB = require("./config/db.js")
 const authRoutes = require("./routes/authRoutes.js")
 const incomeRoutes = require("./routes/incomeRoutes.js")
 const expenseRoutes = require("./routes/expenseRoutes.js")
+const dashboardRoutes = require("./routes/dashboardRoutes.js")
 
 
 // Middleware to handle CORS
@@ -28,6 +29,7 @@ connectDB();
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/income", incomeRoutes);
 app.use("/api/v1/expense", expenseRoutes);
+app.use("/api/v1/dashboard", dashboardRoutes);
 
 // Serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
