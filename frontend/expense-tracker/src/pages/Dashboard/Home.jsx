47bdaaed741410a0a94a76ebcf9b5e067147e4d6
Harsha Vardhan Reddy <harshavennapusa2006@gmail.com>
2025-08-05 23:58:29 +0530

    started with FinanceOverview

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -9,6 +9,7 @@ import { LuHandCoins, LuWalletMinimal } from 'react-icons/lu'
 import { IoMdCard } from "react-icons/io"
 import RecentTransactions from '../../components/Dashboard/RecentTransactions.jsx'
 import { addIndianThousandSeparator } from '../../utils/helper.js'
+import FinanceOverview from '../../components/Dashboard/FinanceOverview.jsx'
 
 const Home = () => {
   useUserAuth()
@@ -72,6 +73,12 @@ const Home = () => {
           <RecentTransactions
             transactions={dashboardData?.recentTransactions}
             onSeeMore={() => navigate("/expense")}
+          />
+
+          <FinanceOverview
+            totalBalance = {dashboardData?.totalBalance || 0}
+            totalIncome = {dashboardData?.totalIncome || 0}
+            totalExpenses = {dashboardData?.totalExpenses || 0}
           />
         </div>
       </div>
